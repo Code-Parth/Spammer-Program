f4785fb4ade4ead5212318fbbec6b79c05b3a474
Code-Parth <softsparths@gmail.com>
2022-08-09 18:21:38 +0530

    Code Uploaded

---
+++ demo.py
@@ -0,0 +1,12 @@
+import random
+import pyautogui
+from time import sleep
+with open("Words.txt", "r") as file:
+    allText = file.read()
+    words = list(map(str, allText.split()))
+sleep(10)
+for i in range(1, 1000):
+    pyautogui.write(random.choice(words))
+    pyautogui.press("enter")
+pyautogui.write("Sorry for spam,I was trying spam program for random words.")
+pyautogui.press("enter")
