8edf5e185b0c3ae1bf00e1b3634ed796b256c334
Code-Parth <softsparths@gmail.com>
2022-08-10 18:01:07 +0530

    Update Program

--- Spammer.py
+++ Spammer.py
@@ -5,7 +5,7 @@ with open("Words.txt", "r") as file:
     allText = file.read()
     words = list(map(str, allText.split()))
 sleep(10)
-for i in range(1, 1000):
+for i in range(0, 1000):
     pyautogui.write(random.choice(words))
     pyautogui.press("enter")
 pyautogui.write("Sorry for spam,I was trying spam program for random words.")
